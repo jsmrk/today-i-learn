3173b219c16bf1d062b23647ce502834dbd60460
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2024-03-25 14:50:39 +0800

    header now in react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./style.css";
+
+function App() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="/logo.png" alt="Today I learned logo" />
+        <h1>Today I Learned</h1>
+      </div>
+
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
+}
+
+export default App;
